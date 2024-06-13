6a5e68261a010b145b71379c2c53f81bd49c8303
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-06-13 18:40:29 +0545

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const promise1 = () => {
+	let text = document.getElementById("text").value;
+	let delay = document.getElementById("delay").value;
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve(text);
+		}, delay*1000);
+	});
+}
+
+async function Submit(){
+	const data = await promise1();
+	console.log(data);
+	document.getElementById("output").innerHTML = `
+	<p>${data}</p>`
+}
